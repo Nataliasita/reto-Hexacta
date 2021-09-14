a4ac66cfab03c4f7445bff0bfdc2af25b93089cd
Natalia Gonzalez <nataliasita04@gmail.com>
2021-09-14 18:45:44 -0500

    form vista catalogo vista

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -4,14 +4,15 @@ import "../estilos/cards.css";
 export function Cards (){
   return (
     <div className ="card_container">
-      <section>
-        <img className ="card_image"/>
-        <h1 className ="card_title"></h1>
-      </section>
-      <p className="card_description"></p>
-      <p className="card_points_value"></p>
-      <button className="card_details">Detalles</button>
-      <button className="card_send_cart">Agregar</button>
+    
+       <img src="" className="image_card" alt=""/>
+         <h1 className ="card_title">{}</h1>
+         <p className="card_description"></p>
+         <p className="card_points_value"></p>
+      <button className="card_details">Agregar a carrito</button>
+      <button className="card_send_cart">Detalle</button>
+
     </div>
+
     )
 }
--- src/components/catalogo.jsx
+++ src/components/catalogo.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import MyHeader from './header'
-import { Footer } from './footer'
+import Form from './formNewProduct'
 import '../estilos/home.css'
 import '../estilos/footer.css'
 import '../estilos/header.css'
 import '../estilos/catalogo.css'
 import {Cards} from './cards'
+import lupa from '../images/lupa.png'
 
 export function Catalogo() {
   return (
     <div className="contain_catalogo">
       <MyHeader/>
         <form action="">
+             <img src={lupa} className="lupa1" alt="" />
              <input className="buscar" placeholder="Buscar producto" type="text"/>
              
         </form>
@@ -20,7 +22,28 @@ export function Catalogo() {
              <div className="products">
               <Cards></Cards>
              </div>
-      <Footer></Footer>
+            
     </div>
   )
+}
+
+
+export function formulario(){
+
+  const [showProductForm, setShowProductForm]= useState(true)
+
+    return (
+        <div>
+            <label htmlFor="">¿No encuentras lo que buscas?</label>
+            <button 
+                type='button'
+                onClick={() => setShowProductForm(!showProductForm)}
+                >Agregar Producto
+            </button>
+            
+            {showProductForm && <Form></Form> }
+            
+        </div>
+    )
+
 }
