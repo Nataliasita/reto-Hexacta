12389e6ab839d3201531c22c4e605105b74b05fb
nelidita <nelidavalero06@gmail.com>
2021-09-16 16:38:41 -0500

    integrando algoritmo de procesamiento de imagen

--- src/components/ProcessingImg.js
+++ src/components/ProcessingImg.js
@@ -1,44 +1,81 @@
-import React,{useRef, useEffect} from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 
-const ProcessingImg = ({image}) => {
+const ProcessingImg = ({ image }) => {
 
-   const canvasEl = useRef(null)
-   
-  
-   useEffect(() => {
-      
-    // console.log(image)
-       const canvas = canvasEl.current
-       const context = canvas.getContext('2d');
-       console.log(context)
+    const canvasEl = useRef(null)
 
-       const img = new Image();
-       img.src = image
-       console.log(img.width)
-       console.log(img.height)
-
-    //    const img = document.getElementById('img');
-
-    canvas.width = img.width;
-    canvas.height = img.height;
-    context.drawImage(img, 0, 0);
-
-    const imageData = context.getImageData(0, 0, img.width, img.height);
-    console.log(imageData.data)
-
-       return () => {
-         
-       }
-   }, [image])
+    const [pixels, setPixels] = useState([]);
+    // const [numPixels, setNumPixels] = useState(0);
 
 
+    useEffect(() => {
 
+        const canvas = canvasEl.current;
+        const context = canvas.getContext('2d');
+
+        const img = new Image();
+        img.src = image
+
+        canvas.width = img.width;
+        canvas.height = img.height;
+        context.drawImage(img, 0, 0);
+
+        const imageData = context.getImageData(0, 0, img.width, img.height);
+        console.log(imageData)
+
+        setPixels(imageData.data);
+        // const numPixels = imageData.width * imageData.height;
+
+        context.putImageData(imageData, 0, 0);
+
+        return () => {
+
+        }
+    }, [image])
+
+    const pixelsUnd = () => {
+        pixels.map(i => {
+            const r = pixels[i * 4];
+            const g = pixels[i * 4 + 1];
+            const b = pixels[i * 4 + 2];
+
+            pixels[i * 4] = r;
+            pixels[i * 4 + 1] = g;
+            pixels[i * 4 + 2] = b;
+
+            // const div = document.createElement("div")
+            // div.className = "divPixel"
+            // container.appendChild(div);
+            // container.style.width = imageData.width + "px";
+            // container.style.height = imageData.height + "px";
+
+            // div.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")"
+        })
+
+    }
 
     return (
         <div>
-            <canvas ref={canvasEl}>
+            <canvas ref={canvasEl}></canvas>
+            <div>
+                {
+                    pixels.map(i => (
 
-            </canvas>
+                        <div key={i}>
+                            
+                        </div>
+
+                            // const div = document.createElement("div")
+                            // div.className = "divPixel"
+                            // container.appendChild(div);
+                            // container.style.width = imageData.width + "px";
+                            // container.style.height = imageData.height + "px";
+
+                            // div.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")"
+                            // )
+                    ))
+                }
+            </div>
         </div>
     )
 }
